ef1cd76f3508fe5d897155ffcce2f79a409abdd2
forex7777 <iharasevinda3@gmail.com>
2025-07-31 16:41:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "xQwygKiK#veCYpGoLwBzZQ2jCRclD-7oNLsmEj581CA9WOmbidbs", // ඔයාගේ session id එක දාන්න
+SESSION_ID: process.env.SESSION_ID || "tcR3BJqZ#iQa4qXqPV4qdnj7NPsS_k1KcZ2auo97k3Dd3-bmLCZw", // ඔයාගේ session id එක දාන්න
 MONGODB: process.env.MONGODB || "mongodb+srv://ashenikavindi00:b1ajNVz40nKVlwjf@cluster0.rvzcqfk.mongodb.net/", // ඔයාගේ mongodb url එක දාන්න
 BOT_NAME: process.env.BOT_NAME || "MD",
 LANG: process.env.BOT_LANG || 'EN' ,
